b1729b1505ce1939bb2493691f0c047cd3e34ea1
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-12-15 19:52:38 -0500

    fix

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ app.post('/', (req, res) => {
 });
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
 app.set('port', process.env.PORT || 5000);
