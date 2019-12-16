f1effe607f5dee88233fbfb4dfe6db09c93d8cea
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-12-15 20:05:30 -0500

    fix

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ const pusher = new Pusher({
   cluster: process.env.PUSHER_APP_CLUSTER,
   useTLS: true,
 });
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -59,16 +67,7 @@ app.post('/', (req, res) => {
         res.send(body);
     });
 });
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/public'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
-  });
-}
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
